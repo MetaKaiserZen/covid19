27601714460f467bb47869494bf01c64a2661c7d
Andrea <a.carreocoppo2@gmail.com>
2021-07-16 01:54:19 -0400

    Mostrando tabla

--- assets/consumoApi.js
+++ assets/consumoApi.js
@@ -1,16 +1,13 @@
-var data= []
-    
    const getData = async () => {
        try{
         const resp = await fetch('http://localhost:3000/api/total');
-        data = await resp.json();        
-        console.log(data);
+        const {data} = await resp.json();  
+        return data      
+        // console.log(data);
        }
         catch(error){
             console.log(error)
         }
-
-        return data
     } 
 
-    export { getData, data}
+    export { getData}
--- assets/scripts.js
+++ assets/scripts.js
@@ -1,1 +1,8 @@
-export {getData} from '/consumoApi.js'
+import { getData} from './consumoApi.js'
+import { paintTable} from './mostrarTabla.js'
+
+// $(document).ready( async function() {
+// })
+
+const datos = await getData();
+paintTable(datos);
